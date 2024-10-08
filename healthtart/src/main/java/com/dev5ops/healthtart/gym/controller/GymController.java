8c5c09a311548895113c7fbf0b3e50ca5d076a86
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-08 14:45:40 +0900

    ✨ 기능 추가 : Gym Controller 기본 세팅 추가

---
+++ healthtart/src/main/java/com/dev5ops/healthtart/gym/controller/GymController.java
@@ -0,0 +1,11 @@
+package com.dev5ops.healthtart.gym.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("gymController")
+@RequestMapping("gym")
+@Slf4j
+public class GymController {
+}
