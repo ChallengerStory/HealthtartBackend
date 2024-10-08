e00722ac4d0aeefcf227f66b6e8f1dae0fc9af75
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-08 12:02:37 +0900

    first commit

---
+++ healthtart/src/main/java/com/dev5ops/healthtart/HealthtartApplication.java
@@ -0,0 +1,13 @@
+package com.dev5ops.healthtart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthtartApplication.class, args);
+    }
+
+}
