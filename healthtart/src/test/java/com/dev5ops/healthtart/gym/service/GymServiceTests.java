0e05127d121526e4db89e705ab04f5f0e6fcedcd
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-08 17:56:45 +0900

    ✨ 기능 수정 : 헬스장 등록, 수정, 삭제 단위 테스트 추가

---
+++ healthtart/src/test/java/com/dev5ops/healthtart/gym/service/GymServiceTests.java
@@ -0,0 +1,262 @@
+package com.dev5ops.healthtart.gym.service;
+
+import com.dev5ops.healthtart.common.exception.CommonException;
+import com.dev5ops.healthtart.common.exception.StatusEnum;
+import com.dev5ops.healthtart.gym.aggregate.Gym;
+import com.dev5ops.healthtart.gym.aggregate.vo.request.RequestEditGymVO;
+import com.dev5ops.healthtart.gym.aggregate.vo.request.RequestRegisterGymVO;
+import com.dev5ops.healthtart.gym.dto.GymDTO;
+import com.dev5ops.healthtart.gym.repository.GymRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class GymServiceTests {
+
+    @Mock
+    private GymRepository gymRepository;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private GymService gymService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("헬스장 등록 성공")
+    @Test
+    void testRegisterGym_Success() {
+        // Given
+        RequestRegisterGymVO request = new RequestRegisterGymVO(
+                "어게인짐 신대방삼거리역",
+                "서울 동작구 상도로 83 2층 어게인짐",
+                "110-81-34859"
+        );
+
+        Gym mockGym = Gym.builder()
+                .gymCode(1L)
+                .gymName(request.getGymName())
+                .address(request.getAddress())
+                .businessNumber(request.getBusinessNumber())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        GymDTO mockGymDTO = new GymDTO(
+                null,
+                request.getGymName(),
+                request.getAddress(),
+                request.getBusinessNumber(),
+                null,
+                null,
+                null
+        );
+
+        when(modelMapper.map(request, GymDTO.class)).thenReturn(mockGymDTO);
+        when(gymRepository.save(any(Gym.class))).thenReturn(mockGym);
+        when(modelMapper.map(mockGym, GymDTO.class)).thenReturn(new GymDTO(
+                1L,
+                request.getGymName(),
+                request.getAddress(),
+                request.getBusinessNumber(),
+                mockGym.getCreatedAt(),
+                mockGym.getUpdatedAt(),
+                null
+        ));
+
+        // When
+        GymDTO responseDTO = gymService.registerGym(mockGymDTO);
+
+        // Then
+        assertNotNull(responseDTO);
+        assertEquals(1L, responseDTO.getGymCode());
+        assertEquals(request.getGymName(), responseDTO.getGymName());
+        verify(gymRepository, times(1)).save(any(Gym.class));
+    }
+
+    @DisplayName("헬스장 등록 실패")
+    @Test
+    void testRegisterGym_DuplicateBusinessNumber() {
+        // Given
+        RequestRegisterGymVO request = new RequestRegisterGymVO(
+                "어게인짐 신대방삼거리역",
+                "서울 동작구 상도로 83 2층 어게인짐",
+                "110-81-34859"
+        );
+
+        // 이미 존재하는 gym을 반환하도록 mock 설정
+        Gym existingGym = Gym.builder()
+                .gymCode(1L)
+                .gymName("어게인짐 신대방삼거리역")
+                .address("서울 동작구 상도로 83 2층 어게인짐")
+                .businessNumber("110-81-34859")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        when(gymRepository.findByBusinessNumber(request.getBusinessNumber())).thenReturn(Optional.of(existingGym));
+
+        // When & Then
+        Exception exception = assertThrows(CommonException.class, () -> {
+            GymDTO mockGymDTO = new GymDTO(
+                    null,
+                    request.getGymName(),
+                    request.getAddress(),
+                    request.getBusinessNumber(),
+                    null,
+                    null,
+                    null
+            );
+            gymService.registerGym(mockGymDTO);
+        });
+
+        // 예외가 StatusEnum.GYM_DUPLICATE인지 확인
+        assertEquals(StatusEnum.GYM_DUPLICATE, ((CommonException) exception).getStatusEnum());
+
+        // gymRepository.save()가 호출되지 않았는지 확인
+        verify(gymRepository, never()).save(any(Gym.class));
+    }
+
+    @DisplayName("헬스장 수정 성공")
+    @Test
+    void testEditGym_Success() {
+        // Given
+        Long gymCode = 1L;
+        RequestEditGymVO request = new RequestEditGymVO(
+                "수정된 헬스장 이름",
+                "수정된 주소",
+                "수정된 사업자번호",
+                new ArrayList<>()
+        );
+
+        Gym existingGym = Gym.builder()
+                .gymCode(gymCode)
+                .gymName("기존 헬스장 이름")
+                .address("기존 주소")
+                .businessNumber("기존 사업자번호")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        when(gymRepository.findById(gymCode)).thenReturn(Optional.of(existingGym));
+
+        Gym updatedGym = Gym.builder()
+                .gymCode(gymCode)
+                .gymName(request.getGymName())
+                .address(request.getAddress())
+                .businessNumber(request.getBusinessNumber())
+                .createdAt(existingGym.getCreatedAt())
+                .updatedAt(LocalDateTime.now())
+                .equipmentPerGyms(request.getEquipmentPerGyms())
+                .build();
+
+        when(gymRepository.save(existingGym)).thenReturn(updatedGym);
+
+        GymDTO updatedGymDTO = new GymDTO(
+                gymCode,
+                updatedGym.getGymName(),
+                updatedGym.getAddress(),
+                updatedGym.getBusinessNumber(),
+                updatedGym.getCreatedAt(),
+                updatedGym.getUpdatedAt(),
+                updatedGym.getEquipmentPerGyms()
+        );
+
+        when(modelMapper.map(updatedGym, GymDTO.class)).thenReturn(updatedGymDTO);
+
+        // When
+        GymDTO result = gymService.editGym(gymCode, request);
+
+        // Then
+        assertNotNull(result);
+        assertEquals("수정된 헬스장 이름", result.getGymName());
+        assertEquals("수정된 주소", result.getAddress());
+        assertEquals("수정된 사업자번호", result.getBusinessNumber());
+        assertNotNull(result.getUpdatedAt());
+        verify(gymRepository, times(1)).save(existingGym);
+    }
+
+    @DisplayName("헬스장 수정 실패 - 헬스장을 찾지 못함")
+    @Test
+    void testEditGym_NotFound() {
+        // Given
+        Long gymCode = 1L;
+        RequestEditGymVO request = new RequestEditGymVO(
+                "수정된 헬스장 이름",
+                "수정된 주소",
+                "수정된 사업자번호",
+                new ArrayList<>()
+        );
+
+        // 헬스장을 찾지 못했을 때, Optional.empty() 반환하도록 설정
+        when(gymRepository.findById(gymCode)).thenReturn(Optional.empty());
+
+        // When & Then
+        CommonException exception = assertThrows(CommonException.class, () -> {
+            gymService.editGym(gymCode, request);
+        });
+
+        assertEquals(StatusEnum.GYM_NOT_FOUND, exception.getStatusEnum());
+
+        // gymRepository.save()는 호출되지 않아야 함
+        verify(gymRepository, never()).save(any(Gym.class));
+    }
+
+
+    @DisplayName("헬스장 삭제 성공")
+    @Test
+    void testDeleteGym_Success() {
+        // Given
+        Long gymCode = 1L;
+        Gym existingGym = Gym.builder()
+                .gymCode(gymCode)
+                .gymName("기존 헬스장 이름")
+                .address("기존 주소")
+                .businessNumber("기존 사업자번호")
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        when(gymRepository.findById(gymCode)).thenReturn(Optional.of(existingGym));
+
+        // When
+        gymService.deleteGym(gymCode);
+
+        // Then
+        verify(gymRepository, times(1)).delete(existingGym);
+    }
+
+    @DisplayName("헬스장 삭제 실패")
+    @Test
+    void testDeleteGym_NotFound() {
+        // Given
+        Long gymCode = 1L;
+
+        when(gymRepository.findById(gymCode)).thenReturn(Optional.empty());
+
+        // When & Then
+        Exception exception = assertThrows(CommonException.class, () -> {
+            gymService.deleteGym(gymCode);
+        });
+
+        assertEquals(StatusEnum.GYM_NOT_FOUND, ((CommonException) exception).getStatusEnum());
+        verify(gymRepository, never()).delete(any(Gym.class));
+    }
+
+}
