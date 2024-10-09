da1ab5968e095de3d98886a8176c259c45847ba8
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-09 21:32:46 +0900

    ✨ 기능 추가 : 운동기구 단 건 조회, 전체 조회 코드 추가

--- healthtart/src/main/java/com/dev5ops/healthtart/exercise_equipment/service/ExerciseEquipmentService.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/exercise_equipment/service/ExerciseEquipmentService.java
@@ -12,6 +12,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -49,5 +51,19 @@ public class ExerciseEquipmentService {
         ExerciseEquipment exerciseEquipment = exerciseEquipmentRepository.findById(exerciseEquipmentCode).orElseThrow(() -> new CommonException(StatusEnum.EQUIPMENT_NOT_FOUND));
 
         exerciseEquipmentRepository.delete(exerciseEquipment);
+    }
+
+    public ExerciseEquipmentDTO findEquipmentByEquipmentCode(Long exerciseEquipmentCode) {
+        ExerciseEquipment exerciseEquipment = exerciseEquipmentRepository.findById(exerciseEquipmentCode).orElseThrow(() -> new CommonException(StatusEnum.EQUIPMENT_NOT_FOUND));
+
+        return modelMapper.map(exerciseEquipment, ExerciseEquipmentDTO.class);
+    }
+
+    public List<ExerciseEquipmentDTO> findAllEquipment() {
+        List<ExerciseEquipment> exerciseEquipments = exerciseEquipmentRepository.findAll();
+
+        return exerciseEquipments.stream()
+                .map(exerciseEquipment -> modelMapper.map(exerciseEquipment, ExerciseEquipmentDTO.class))
+                .collect(Collectors.toList());
     }
 }
