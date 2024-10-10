dda810ecc2e982ba883825bf6dcdea3e443e3b15
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-10 10:38:41 +0900

    ✨ 기능 추가 : Routine 수정 VO 기능 추가

---
+++ healthtart/src/main/java/com/dev5ops/healthtart/routine/domain/vo/EditRoutineVO.java
@@ -0,0 +1,14 @@
+package com.dev5ops.healthtart.routine.domain.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class EditRoutineVO {
+    private String title;
+    private Integer time;
+}
