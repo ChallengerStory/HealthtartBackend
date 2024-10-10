9b818d5c9b14ec09bdc9520c3350c45332b8e783
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-10 12:44:02 +0900

    ✨ 기능 추가 : 인바디 수정, 조회를 위한 VO 추가, Gym VO에서의 불필요한 주석 삭제

--- healthtart/src/main/java/com/dev5ops/healthtart/gym/aggregate/vo/request/RequestEditGymVO.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/gym/aggregate/vo/request/RequestEditGymVO.java
@@ -1,11 +1,8 @@
 package com.dev5ops.healthtart.gym.aggregate.vo.request;
 
-import com.dev5ops.healthtart.equipment_per_gym.aggregate.EquipmentPerGym;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
--- healthtart/src/main/java/com/dev5ops/healthtart/gym/aggregate/vo/response/ResponseEditGymVO.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/gym/aggregate/vo/response/ResponseEditGymVO.java
@@ -1,12 +1,10 @@
 package com.dev5ops.healthtart.gym.aggregate.vo.response;
 
-import com.dev5ops.healthtart.equipment_per_gym.aggregate.EquipmentPerGym;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
