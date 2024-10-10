37bf8ea74b17ea2d1263551f0b31bdf74d8e09ce
Hellin22 <dno06117@naver.com>
2024-10-10 16:05:44 +0900

    ✨ 기능 추가 / 수정: user find 메서드 추가 및 필요없는 import 제거

--- healthtart/src/main/java/com/dev5ops/healthtart/user/service/UserService.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/user/service/UserService.java
@@ -1,25 +1,20 @@
 package com.dev5ops.healthtart.user.service;
 
 import com.dev5ops.healthtart.user.domain.dto.UserDTO;
-import com.dev5ops.healthtart.user.domain.entity.User;
 import com.dev5ops.healthtart.user.domain.vo.request.RequestInsertUserVO;
 import com.dev5ops.healthtart.user.domain.vo.response.ResponseInsertUserVO;
-import com.dev5ops.healthtart.user.repository.UserRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public interface UserService extends UserDetailsService {
-    public ResponseInsertUserVO signUpUser(RequestInsertUserVO request);
+    ResponseInsertUserVO signUpUser(RequestInsertUserVO request);
 
-    public List<UserDTO> findAllUsers();
+    List<UserDTO> findAllUsers();
+
+    UserDTO findUserByEmail(String email);
+
+    UserDTO findById(String userCode);
 }
