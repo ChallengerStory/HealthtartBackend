be8aade0db0e6e3288456fa286199e299dc1db04
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-10-10 14:52:29 +0900

    ✨ 기능 추가 : 인바디 정보 조회 기능 테스트 코드 추가

--- healthtart/src/test/java/com/dev5ops/healthtart/inbody/service/InbodyServiceTests.java
+++ healthtart/src/test/java/com/dev5ops/healthtart/inbody/service/InbodyServiceTests.java
@@ -18,7 +18,10 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -337,5 +340,179 @@ class InbodyServiceTests {
         verify(inbodyRepository, times(1)).delete(existingInbody);
     }
 
+    @DisplayName("인바디 단 건 조회 성공")
+    @Test
+    void testFindInbodyByCode_Success() {
+        // Given
+        Long inbodyCode = 1L;
 
+        Inbody existingInbody = Inbody.builder()
+                .inbodyCode(inbodyCode)
+                .inbodyScore(85)
+                .weight(70.0)
+                .height(175.0)
+                .muscleWeight(30.0)
+                .fatWeight(15.0)
+                .bmi(22.9)
+                .fatPercentage(18.0)
+                .dayOfInbody(LocalDateTime.now().minusDays(1))
+                .basalMetabolicRate(1600)
+                .createdAt(LocalDateTime.now().minusDays(5))
+                .updatedAt(LocalDateTime.now().minusDays(5))
+                .user(User.builder()
+                        .userCode("test")
+                        .userType(UserTypeEnum.MEMBER)
+                        .userName("testuser")
+                        .userEmail("testuser@example.com")
+                        .userPassword("testpassword")
+                        .userPhone("010-1234-5678")
+                        .nickname("tester")
+                        .userAddress("testAddress")
+                        .userFlag(true)
+                        .userGender("Male")
+                        .userHeight(175.0)
+                        .userWeight(70.0)
+                        .userAge(25)
+                        .createdAt(LocalDateTime.now().minusDays(10))
+                        .updatedAt(LocalDateTime.now().minusDays(5))
+                        .gymCode(100L)
+                        .build())
+                .build();
+
+        when(inbodyRepository.findById(inbodyCode)).thenReturn(Optional.of(existingInbody));
+
+        InbodyDTO expectedDTO = new InbodyDTO(
+                inbodyCode,
+                85,
+                70.0,
+                175.0,
+                30.0,
+                15.0,
+                22.9,
+                18.0,
+                LocalDateTime.now().minusDays(1),
+                1600,
+                LocalDateTime.now().minusDays(5),
+                LocalDateTime.now().minusDays(5),
+                existingInbody.getUser()
+        );
+
+        when(modelMapper.map(existingInbody, InbodyDTO.class)).thenReturn(expectedDTO);
+
+        // When
+        InbodyDTO result = inbodyService.findInbodyByCode(inbodyCode);
+
+        // Then
+        assertNotNull(result, "The result should not be null.");
+        assertEquals(85, result.getInbodyScore());
+        assertEquals(70.0, result.getWeight());
+        assertEquals(175.0, result.getHeight());
+        verify(inbodyRepository, times(1)).findById(inbodyCode);
+
+        System.out.println("expectedDTO: " + expectedDTO);
+        System.out.println("result: " + result);
+    }
+
+    @DisplayName("인바디 전체 조회 성공")
+    @Test
+    void testFindAllInbody_Success() {
+        // Given
+        List<Inbody> inbodyList = Arrays.asList(
+                Inbody.builder()
+                        .inbodyCode(1L)
+                        .inbodyScore(85)
+                        .weight(70.0)
+                        .height(175.0)
+                        .muscleWeight(30.0)
+                        .fatWeight(15.0)
+                        .bmi(22.9)
+                        .fatPercentage(18.0)
+                        .dayOfInbody(LocalDateTime.now().minusDays(1))
+                        .basalMetabolicRate(1600)
+                        .createdAt(LocalDateTime.now().minusDays(5))
+                        .updatedAt(LocalDateTime.now().minusDays(5))
+                        .user(User.builder()
+                                .userCode("test1")
+                                .userType(UserTypeEnum.MEMBER)
+                                .userName("testuser1")
+                                .userEmail("testuser1@example.com")
+                                .userPassword("testpassword1")
+                                .userPhone("010-1234-5678")
+                                .nickname("tester1")
+                                .userAddress("testAddress1")
+                                .userFlag(true)
+                                .userGender("Male")
+                                .userHeight(175.0)
+                                .userWeight(70.0)
+                                .userAge(25)
+                                .createdAt(LocalDateTime.now().minusDays(10))
+                                .updatedAt(LocalDateTime.now().minusDays(5))
+                                .gymCode(100L)
+                                .build())
+                        .build(),
+                Inbody.builder()
+                        .inbodyCode(2L)
+                        .inbodyScore(90)
+                        .weight(80.0)
+                        .height(180.0)
+                        .muscleWeight(35.0)
+                        .fatWeight(20.0)
+                        .bmi(24.7)
+                        .fatPercentage(20.0)
+                        .dayOfInbody(LocalDateTime.now().minusDays(2))
+                        .basalMetabolicRate(1800)
+                        .createdAt(LocalDateTime.now().minusDays(6))
+                        .updatedAt(LocalDateTime.now().minusDays(6))
+                        .user(User.builder()
+                                .userCode("test2")
+                                .userType(UserTypeEnum.MEMBER)
+                                .userName("testuser2")
+                                .userEmail("testuser2@example.com")
+                                .userPassword("testpassword2")
+                                .userPhone("010-8765-4321")
+                                .nickname("tester2")
+                                .userAddress("testAddress2")
+                                .userFlag(true)
+                                .userGender("Female")
+                                .userHeight(180.0)
+                                .userWeight(80.0)
+                                .userAge(30)
+                                .createdAt(LocalDateTime.now().minusDays(10))
+                                .updatedAt(LocalDateTime.now().minusDays(5))
+                                .gymCode(200L)
+                                .build())
+                        .build()
+        );
+
+        when(inbodyRepository.findAll()).thenReturn(inbodyList);
+
+        when(modelMapper.map(any(Inbody.class), eq(InbodyDTO.class))).thenAnswer(invocation -> {
+            Inbody inbody = invocation.getArgument(0);
+            return new InbodyDTO(
+                    inbody.getInbodyCode(),
+                    inbody.getInbodyScore(),
+                    inbody.getWeight(),
+                    inbody.getHeight(),
+                    inbody.getMuscleWeight(),
+                    inbody.getFatWeight(),
+                    inbody.getBmi(),
+                    inbody.getFatPercentage(),
+                    inbody.getDayOfInbody(),
+                    inbody.getBasalMetabolicRate(),
+                    inbody.getCreatedAt(),
+                    inbody.getUpdatedAt(),
+                    inbody.getUser()
+            );
+        });
+
+        // When
+        List<InbodyDTO> result = inbodyService.findAllInbody();
+
+        // Then
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(85, result.get(0).getInbodyScore());
+        assertEquals(90, result.get(1).getInbodyScore());
+        verify(inbodyRepository, times(1)).findAll();
+    }
 }
