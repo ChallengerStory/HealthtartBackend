b59f62be4b0e03dd6cd64df6ba4fb541ff1dbf36
YuJeeun <dbwpdms1122@naver.com>
2024-10-12 20:28:32 +0900

    ✨ 기능 추가: RecommendedWorkoutHistoryService 만족도순 조회 추가

--- healthtart/src/main/java/com/dev5ops/healthtart/recommended_workout_history/service/RecommendedWorkoutHistoryService.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/recommended_workout_history/service/RecommendedWorkoutHistoryService.java
@@ -1,5 +1,7 @@
 package com.dev5ops.healthtart.recommended_workout_history.service;
 
+import com.dev5ops.healthtart.common.exception.CommonException;
+import com.dev5ops.healthtart.common.exception.StatusEnum;
 import com.dev5ops.healthtart.recommended_workout_history.domain.dto.RecommendedWorkoutHistoryDTO;
 import com.dev5ops.healthtart.recommended_workout_history.domain.entity.RecommendedWorkoutHistory;
 import com.dev5ops.healthtart.recommended_workout_history.domain.vo.request.RequestRegisterRecommendedWorkoutHistoryVO;
@@ -18,6 +20,19 @@ import java.util.List;
 public class RecommendedWorkoutHistoryService {
     private final RecommendedWorkoutHistoryRepository recommendedWorkoutHistoryRepository;
     private final ModelMapper modelMapper;
+
+    // 운동추천내역 만족도순 조회 1. 운동루틴별 운동추천내역 조회 2. 만족도만 다 더해서 평균내기
+    public double findByRatingOrder(Long routineCode) {
+        List<RecommendedWorkoutHistoryDTO> recommendedWorkoutHistoryDTOList = recommendedWorkoutHistoryRepository
+                .findByRoutineCode(routineCode);
+
+        return recommendedWorkoutHistoryDTOList.stream()
+                .mapToDouble(RecommendedWorkoutHistoryDTO::getRoutineRatings)
+                .average()
+                .orElseThrow(() -> new CommonException(StatusEnum.ROUTINE_NOT_FOUND));
+    }
+
+
 
     // 만족도 등록
     public RecommendedWorkoutHistoryDTO registerRating
