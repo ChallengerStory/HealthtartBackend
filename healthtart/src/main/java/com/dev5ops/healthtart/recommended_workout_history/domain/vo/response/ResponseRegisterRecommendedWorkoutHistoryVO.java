4b837c696b0292d63bd785aa9c4bfa85deb5bf21
YuJeeun <dbwpdms1122@naver.com>
2024-10-13 00:17:45 +0900

    ✨ 기능 추가: ResponseRegisterRecommendedWorkoutHistoryVO 추가

---
+++ healthtart/src/main/java/com/dev5ops/healthtart/recommended_workout_history/domain/vo/response/ResponseRegisterRecommendedWorkoutHistoryVO.java
@@ -0,0 +1,19 @@
+package com.dev5ops.healthtart.recommended_workout_history.domain.vo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ResponseRegisterRecommendedWorkoutHistoryVO {
+    private Long historyCode;
+    private int routineRatings;
+    private int recommendedTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long routineCode;
+}
