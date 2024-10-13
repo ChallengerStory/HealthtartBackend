704c55d56f86de8ba9b766f908046ccd2791e318
Hellin22 <dno06117@naver.com>
2024-10-13 16:01:49 +0900

    ♻️ 코드 수정: Rival crud를 위한 기본 틀 구성(controller, dto)

--- healthtart/src/main/java/com/dev5ops/healthtart/rival/controller/RivalController.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/rival/controller/RivalController.java
@@ -1,4 +1,10 @@
 package com.dev5ops.healthtart.rival.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RivalController {
+
+    // 1. 라이벌 등록하는 기능
+
 }
