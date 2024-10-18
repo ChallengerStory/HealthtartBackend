a528dbf4fb22b4728bb77b488e0d8dd0ad5c0585
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-18 14:39:38 +0900

    ✨ 기능 수정/추가 : 운동루틴 추천 GPT API 데이터 저장 및 등록 기능 수정/추가

--- healthtart/src/main/java/com/dev5ops/healthtart/gpt/service/GptService.java
+++ healthtart/src/main/java/com/dev5ops/healthtart/gpt/service/GptService.java
@@ -1,5 +1,9 @@
 package com.dev5ops.healthtart.gpt.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public interface GptService {
     String generateRoutine(String userCode, String bodyPart, int time);
+
+    void processRoutine(String response) throws JsonProcessingException;
 }
