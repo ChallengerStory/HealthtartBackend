bad3edd67eb43314fa794f391b1fcafcb2fe9377
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-10-18 14:06:54 +0900

    ✨ 기능 추가 : GptService 유저 정보와 헬스기구 정보를 통한 GPT 루틴 생성 후 3개 도메인에 저장 테스트 기능 추가

--- healthtart/src/test/java/com/dev5ops/healthtart/gpt/service/GptServiceImplTests.java
+++ healthtart/src/test/java/com/dev5ops/healthtart/gpt/service/GptServiceImplTests.java
@@ -2,12 +2,21 @@ package com.dev5ops.healthtart.gpt.service;
 
 import com.dev5ops.healthtart.exercise_equipment.domain.dto.ExerciseEquipmentDTO;
 import com.dev5ops.healthtart.exercise_equipment.service.ExerciseEquipmentService;
+import com.dev5ops.healthtart.routine.domain.dto.RoutineDTO;
+import com.dev5ops.healthtart.routine.service.RoutineService;
 import com.dev5ops.healthtart.user.domain.dto.UserDTO;
 import com.dev5ops.healthtart.user.service.UserService;
+import com.dev5ops.healthtart.workout_per_routine.domain.dto.WorkoutPerRoutineDTO;
+import com.dev5ops.healthtart.workout_per_routine.service.WorkoutPerRoutineService;
+import com.dev5ops.healthtart.workoutinfo.domain.dto.WorkoutInfoDTO;
+import com.dev5ops.healthtart.workoutinfo.service.WorkoutInfoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -18,18 +27,32 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class GptServiceImplTests {
 
-    @Autowired
-    private GptServiceImpl gptServiceImpl;
 
     @Autowired
     private UserService userService;
 
     @Autowired
+    @Mock
     private ExerciseEquipmentService exerciseEquipmentService;
+
+    @Autowired
+    @InjectMocks
+    private GptServiceImpl gptServiceImpl;
+
+    @Mock
+    private RoutineService routineService;
+
+    @Mock
+    private WorkoutPerRoutineService workoutPerRoutineService;
+
+    @Mock
+    private WorkoutInfoService workoutInfoService;
+
 
     private String userCode;
     private String bodyPart;
     private int workoutTime;
+    private String gptResponse;
 
     @BeforeEach
     public void setUp() {
@@ -39,7 +62,7 @@ class GptServiceImplTests {
     }
 
     @Test
-    @DisplayName("유저 정보와 헬스기구 정보를 통한 GPT 루틴 생성 테스트")
+    @DisplayName("유저 정보와 헬스기구 정보를 통한 GPT 루틴 생성 후 3개 도메인에 저장 테스트")
     public void testGenerateRoutine() throws JsonProcessingException {
         UserDTO userDTO = userService.findById(userCode);
         assertNotNull(userDTO, "유저 정보를 찾을 수 없습니다.");
@@ -52,5 +75,9 @@ class GptServiceImplTests {
         assertNotNull(response, "GPT 응답이 null입니다.");
         assertTrue(response.contains("하체"), "응답에 운동 부위 정보가 포함되지 않았습니다.");
         assertTrue(response.contains("레그 프레스"), "응답에 레그 프레스 정보가 포함되지 않았습니다.");
+
+        gptServiceImpl.processRoutine(response);
+
     }
+
 }
